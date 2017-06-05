8b728fe6ed746f6a1dc877b1684c2a0305b9f9d9
ivansivoplias <ivan.kozaks@gmail.com>
2017-06-05 22:46:56 +0300

    Add some changes to UnitOfWork class.

--- Organizer.DAL/UnitOfWork/UnitOfWork.cs
+++ Organizer.DAL/UnitOfWork/UnitOfWork.cs
@@ -2,50 +2,58 @@
 using Organizer.Infrastructure;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
 
 namespace Organizer.DAL.UnitOfWork
 {
     public class UnitOfWork : IUnitOfWork
     {
-        private Dictionary<string, object> _repositories;
+        private IDbTransaction _transaction;
+        private readonly Action<IUnitOfWork> _rolledBack;
+        private readonly Action<IUnitOfWork> _committed;
         private IRepositoryProvider _repositoryProvider;
 
-        public UnitOfWork()
+        public UnitOfWork(IDbTransaction transaction, Action<IUnitOfWork> rolledBack, Action<IUnitOfWork> committed)
         {
+            Transaction = transaction;
+            _rolledBack = rolledBack;
+            _committed = committed;
             _repositoryProvider = new RepositoryProvider();
+        }
+
+        public IDbTransaction Transaction
+        {
+            get { return _transaction; }
+            private set { _transaction = value; }
         }
 
         public IRepository<TEntity> GetRepository<TEntity>() where TEntity : IEntity
         {
-            IRepository<TEntity> result = null;
-
-            if (_repositories == null)
-            {
-                _repositories = new Dictionary<string, object>();
-            }
-
             var repositoryType = typeof(IRepository<TEntity>);
             var repositoryName = repositoryType.Name;
 
-            if (_repositories.ContainsKey(repositoryName))
-            {
-                result = (IRepository<TEntity>)_repositories[repositoryName];
-            }
-            else
-            {
-                var repository = _repositoryProvider.GetRepositoryForKey<TEntity>(repositoryName);
-                _repositories[repositoryName] = repository;
-            }
-
-            return result;
+            return _repositoryProvider.GetRepositoryForKey<TEntity>(repositoryName);
         }
 
         public void SaveChanges()
         {
-            throw new NotImplementedException();
+            if (_transaction == null)
+                throw new InvalidOperationException("May not call save changes twice.");
+
+            _transaction.Commit();
+            _committed(this);
+            _transaction = null;
+        }
+
+        public void Dispose()
+        {
+            if (_transaction == null)
+                return;
+
+            _transaction.Rollback();
+            _transaction.Dispose();
+            _rolledBack(this);
+            _transaction = null;
         }
     }
 }
--- Organizer.Infrastructure/IUnitOfWork.cs
+++ Organizer.Infrastructure/IUnitOfWork.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Organizer.Infrastructure
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
         IRepository<TEntity> GetRepository<TEntity>() where TEntity : IEntity;
 
