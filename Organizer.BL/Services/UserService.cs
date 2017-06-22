35c0a553dd0b786a9d2a2ba5b56456a40c031248
ivansyvoplias <ivan.kozaks@gmail.com>
2017-06-22 19:58:43 +0300

    Add default user service implementation.

---
+++ Organizer.BL/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using Organizer.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Organizer.Common.Entities;
+using Organizer.Infrastructure.Database;
+
+namespace Organizer.BL.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IDatabaseContextFactory _contextFactory;
+
+        public UserService(IDatabaseContextFactory factory)
+        {
+            _contextFactory = factory;
+        }
+
+        public User Login(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Register(User newUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
