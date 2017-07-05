d92836d3be20015738b67751ed1b33249aa316b6
ivansivoplias <ivan.kozaks@gmail.com>
2017-07-05 22:10:02 +0300

    Add icon. Remove login restriction.

--- Organizer.UI/ValidationRules/User/LoginValidationRule.cs
+++ Organizer.UI/ValidationRules/User/LoginValidationRule.cs
@@ -6,10 +6,6 @@ namespace Organizer.UI.ValidationRules
 {
     public class LoginValidationRule : ValidationRule
     {
-        public int MinLoginLength { get; set; }
-
-        public int MaxLoginLength { get; set; }
-
         private static readonly Regex _loginRegex =
             new Regex("^(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$", RegexOptions.Compiled);
 
@@ -20,13 +16,9 @@ namespace Organizer.UI.ValidationRules
 
             var login = value as string;
 
-            if (login.Length > MaxLoginLength || login.Length < MinLoginLength)
-            {
-                return new ValidationResult(false, $"Login cannot be less than {MinLoginLength} and greater than {MaxLoginLength} symbols.");
-            }
-
             if (!_loginRegex.IsMatch(login))
-                return new ValidationResult(false, "Login contains forbidden symbols. Please use english letters, digits . and _.");
+                return new ValidationResult(false,
+                    "Login contains forbidden symbols. Please use english letters, digits . and _.");
 
             return ValidationResult.ValidResult;
         }
